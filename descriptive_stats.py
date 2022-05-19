1cc8e7962b3acb7d1d46e37b25668858e2a340b2
tcaouette-om1 <82820698+tcaouette-om1@users.noreply.github.com>
2022-05-19 07:38:06 -0700

    dictionary build done

--- descriptive_stats.py
+++ descriptive_stats.py
@@ -59,16 +59,54 @@ print(table_names)
 
 # query each table in list  --> create df for each table --> find the dtype of each field in the table and run a stats query on each field in table
 
-# this gets the column names to re-name each table...
+# need to create the dictionary here ---  {table_name:[columns]}   # this gets the column names to re-name each table...
 table_col =['table_name',	'schema_name',	'column_name',	'data_type',	'null',	'default',	'kind',	'expression',	'comment',	'database_name',	'autoincrement']
 df_all_list =[]
 for table in table_names:
     select_columns = f'''show columns in table {table}'''
-df = fetch_pandas_old(cs_id,select_columns)
-df.columns=table_col
+    df_all_list.append(fetch_pandas_old(cs_id,select_columns))
+#df.columns=table_col
 
+#normalize and apply the column headers to the df so that is in the correct format.
+norm_df_list =[]
+for df in df_all_list:
+    df.columns = table_col
+    norm_df_list.append(df)
 #create dictionary that has table name the list of column names... apply at will
+#column names are needed to rename the columns in the next section. without these lists the headers are not in a good format.
 
     #df_all_list.append(fetch_pandas_old(cs_id,select_all))
 
-print(df)
+print(norm_df_list)
+#creates list of lists, those lists are the column names of every ---> should create dictionary with key being table name values list of column names
+list_columns =[]
+for df in norm_df_list:
+    list_columns.append(df['column_name'].tolist())
+
+
+#print(list_columns)
+
+#dictionary!
+table_col_dict = {}
+for k, v in zip(table_names, list_columns):
+   table_col_dict.setdefault(k, []).append(v)
+
+print(table_col_dict['ENCOUNTER'][0])
+# the calculation of the descriptive stats can be done with python or sql... will mostlikely go with python and iterating through each df will be easier that way
+
+all_sql = f'''
+select * from {table} limit 10
+'''
+
+
+
+
+#continuous_sql = f'''
+#SELECT MIN({column}) AS value_min, 
+#       MAX({column}) AS value_max, 
+#       AVG({column}) AS value_avg, 
+#       STDDEV({column}) AS value_stddev, 
+#       VAR({column}) AS value_var
+#FROM {table}
+#WHERE {column} IS NOT NULL
+#'''
